fbc009f4f0388d57f556a474ac33d98720f6ff9f
Ondřej Pražák <xprazak2@users.noreply.github.com>
2019-06-18 15:23:51 +0200

    Restore ruby 2.0 compatibility (#2)

--- lib/openscap_parser/ds.rb
+++ lib/openscap_parser/ds.rb
@@ -17,9 +17,12 @@ module OpenscapParser
 
     def profile_nodes
       @report_xml.xpath(".//Profile").map do |node|
-        id = node.attribute('id')&.value
-        title = node.at_xpath('./title')&.text
-        description = node.at_xpath('./description')&.text
+        id_node = node.attribute('id')
+        id = id_node.value if id_node
+        title_node = node.at_xpath('./title')
+        title = title_node.text if title_node
+        desc_node = node.at_xpath('./description')
+        description = desc_node.text if desc_node
         { :id => id, :title => title, :description => description }
       end
     end
