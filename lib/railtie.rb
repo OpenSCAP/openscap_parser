e701efc9fd1e6ff911342205bedd81730678d103
Andrew Kofink <akofink@redhat.com>
2019-09-26 10:35:05 -0400

    Add rake tasks when included with rails
    Signed-off-by: Andrew Kofink <akofink@redhat.com>

---
+++ lib/railtie.rb
@@ -0,0 +1,15 @@
+# lib/railtie.rb
+require 'openscap_parser'
+
+if defined?(Rails)
+  module OpenscapParser
+    class Railtie < Rails::Railtie
+      railtie_name :openscap_parser
+
+      rake_tasks do
+        path = File.expand_path(__dir__)
+        Dir.glob("#{path}/tasks/**/*.rake").each { |f| load f }
+      end
+    end
+  end
+end
