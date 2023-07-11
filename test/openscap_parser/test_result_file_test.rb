380af9a323975e342ddde2a4337536a9382260a8
Jan Rodak <hony.com@seznam.cz>
2023-07-11 14:24:48 +0200

    Add tests that Verify the parser works the same with ARF and XCCDF files

--- test/openscap_parser/test_result_file_test.rb
+++ test/openscap_parser/test_result_file_test.rb
@@ -416,6 +416,294 @@ class TestResultFileTest < Minitest::Test
     end
   end
 
+  context 'Verify the parser works the same with ARF and XCCDF files' do
+    {
+      'xccdf' => OpenscapParser::TestResultFile.new(file_fixture('ssg-rhel9-ds-xccdf.results.xml').read),
+      'arf' => OpenscapParser::TestResultFile.new(file_fixture('ssg-rhel9-ds-arf.xml').read)
+    }.each do |report_type, report|
+      context 'benchmark' do
+        test "#{report_type}_report_description" do
+          assert_match(/^This guide presents/, report.benchmark.description)
+        end
+
+        test "#{report_type}_profile_id" do
+          assert_equal 'xccdf_org.ssgproject.content_profile_anssi_bp28_enhanced', report.benchmark.profiles.first.id
+        end
+
+        context 'profiles' do
+          test "#{report_type}_profile_selected_rule_ids" do
+            assert_equal(163, report.benchmark.profiles.first.selected_rule_ids.length)
+            refute_includes(report.benchmark.profiles.first.selected_rule_ids,
+                            'xccdf_org.ssgproject.rules_group_crypto')
+            refute_includes(report.benchmark.profiles.first.selected_rule_ids,
+                            'xccdf_org.ssgproject.content_group_rule_crypto')
+            refute_includes(report.benchmark.profiles.first.selected_rule_ids,
+                            'xccdf_org.ssgproject.contentrule_group_crypto')
+            refute_includes(report.benchmark.profiles.first.selected_rule_ids,
+                            'xccdf_org.ssgproject.content_group_rule_group_crypto')
+          end
+
+          test "#{report_type}_profile_unselected_group_ids" do
+            assert_equal(190, report.benchmark.profiles.first.unselected_group_ids.count)
+            assert_includes(report.benchmark.profiles.first.unselected_group_ids,
+                            'xccdf_org.ssgproject.content_group_accounts-banners')
+            assert_includes(report.benchmark.profiles.first.unselected_group_ids,
+                            'xccdf_org.ssgproject.content_group_accounts-physical')
+            assert_includes(report.benchmark.profiles.first.unselected_group_ids,
+                            'xccdf_org.ssgproject.content_group_ftp')
+            assert_includes(report.benchmark.profiles.first.unselected_group_ids,
+                            'xccdf_org.ssgproject.content_group_gnome_media_settings')
+          end
+
+          test "#{report_type}_profile_refined_values" do
+            assert_equal(
+              {
+                'xccdf_org.ssgproject.content_value_var_password_pam_unix_rounds' => '65536',
+                'xccdf_org.ssgproject.content_value_var_accounts_maximum_age_login_defs' => '90',
+                'xccdf_org.ssgproject.content_value_var_password_pam_minlen' => '18',
+                'xccdf_org.ssgproject.content_value_var_password_pam_ocredit' => '1',
+                'xccdf_org.ssgproject.content_value_var_password_pam_dcredit' => '1',
+                'xccdf_org.ssgproject.content_value_var_password_pam_ucredit' => '1',
+                'xccdf_org.ssgproject.content_value_var_password_pam_lcredit' => '1',
+                'xccdf_org.ssgproject.content_value_var_accounts_passwords_pam_faillock_fail_interval' => '900',
+                'xccdf_org.ssgproject.content_value_var_accounts_passwords_pam_faillock_deny' => '3',
+                'xccdf_org.ssgproject.content_value_var_password_pam_tally2' => '5',
+                'xccdf_org.ssgproject.content_value_var_accounts_passwords_pam_faillock_unlock_time' => '900',
+                'xccdf_org.ssgproject.content_value_var_password_pam_remember' => '2',
+                'xccdf_org.ssgproject.content_value_var_password_pam_remember_control_flag' => 'requisite',
+                'xccdf_org.ssgproject.content_value_var_authselect_profile' => 'sssd',
+                'xccdf_org.ssgproject.content_value_var_sudo_umask' => '0027',
+                'xccdf_org.ssgproject.content_value_var_sudo_passwd_timeout' => '1_minute',
+                'xccdf_org.ssgproject.content_value_var_sudo_dedicated_group' => 'sudogrp',
+                'xccdf_org.ssgproject.content_value_var_polyinstantiation_enabled' => 'on',
+                'xccdf_org.ssgproject.content_value_sysctl_net_ipv4_conf_all_accept_redirects_value' => 'disabled',
+                'xccdf_org.ssgproject.content_value_sysctl_net_ipv4_conf_default_accept_redirects_value' => 'disabled',
+                'xccdf_org.ssgproject.content_value_var_selinux_policy_name' => 'targeted',
+                'xccdf_org.ssgproject.content_value_var_accounts_user_umask' => '077',
+                'xccdf_org.ssgproject.content_value_var_accounts_tmout' => '10_min',
+                'xccdf_org.ssgproject.content_value_sshd_idle_timeout_value' => '10_minutes',
+                'xccdf_org.ssgproject.content_value_var_selinux_state' => 'enforcing'
+              },
+              report.benchmark.profiles.first.refined_values
+            )
+          end
+        end
+
+        context 'groups' do
+          test "#{report_type}_group_id" do
+            assert_match(/^xccdf_org.ssgproject.content_group_system/,
+                         report.benchmark.groups.first.id)
+          end
+
+          test "#{report_type}_group_no_conflicts" do
+            assert_equal([], report.benchmark.groups.first.conflicts)
+          end
+
+          test "#{report_type}_group_no_requires" do
+            assert_equal([], report.benchmark.groups[1].requires)
+          end
+
+          test "#{report_type}_group_description" do
+            assert_match(/^Contains rules that check correct system settings./,
+                         report.benchmark.groups.first.description)
+          end
+
+          test "#{report_type}_group_parent_id_benchmark" do
+            assert_match(/^xccdf_org.ssgproject.content_benchmark_RHEL-9/,
+                         report.benchmark.groups.first.parent_id)
+          end
+
+          test "#{report_type}_group_parent_id_group" do
+            assert_match(/^xccdf_org.ssgproject.content_group_system/,
+                         report.benchmark.groups[1].parent_id)
+          end
+
+          test "#{report_type}_group_parent_type_with_benchmark_parent" do
+            assert_match(/^Benchmark/,
+                         report.benchmark.groups.first.parent_type)
+          end
+
+          test "#{report_type}_group_parent_type_with_group_parent" do
+            assert_match(/^Group/,
+                         report.benchmark.groups[1].parent_type)
+          end
+        end
+
+        context 'rules' do
+          test "#{report_type}_list all rules" do
+            arbitrary_rules = [
+              'xccdf_org.ssgproject.content_rule_bios_enable_execution_restrictions',
+              'xccdf_org.ssgproject.content_rule_selinux_all_devicefiles_labeled'
+            ]
+            assert_empty(
+              arbitrary_rules - report.benchmark.rules.map(&:id)
+            )
+          end
+
+          test "#{report_type}_removes newlines from rule description" do
+            rule = report.benchmark.rules.find do |r|
+              r.id == 'xccdf_org.ssgproject.content_rule_service_atd_disabled'
+            end
+
+            desc = <<~DESC.gsub("\n", ' ').strip
+              The at and batch commands can be used to
+              schedule tasks that are meant to be executed only once. This allows delayed
+              execution in a manner similar to cron, except that it is not
+              recurring. The daemon atd keeps track of tasks scheduled via
+              at and batch, and executes them at the specified time.
+              The atd service can be disabled with the following command:
+              $ sudo systemctl mask --now atd.service
+            DESC
+
+            assert_equal desc, rule.description
+          end
+
+          test "#{report_type}_rule_id" do
+            assert_match(/^xccdf_org.ssgproject.content_rule_prefer_64bit_os/,
+                         report.benchmark.rules.first.id)
+          end
+          test "#{report_type}_rule_no_conflicts" do
+            assert_equal([], report.benchmark.rules[1].conflicts)
+          end
+
+          test "#{report_type}_rule_no_requires" do
+            assert_equal([], report.benchmark.rules.first.requires)
+          end
+
+          test "#{report_type}_rule_description" do
+            assert_match(/^Without cryptographic integrity protections,/,
+                         report.benchmark.rules[1].description)
+          end
+          test "#{report_type}_rule_parent_id_benchmark" do
+            assert_match(/^xccdf_org.ssgproject.content_group_software/,
+                         report.benchmark.rules.first.parent_id)
+          end
+          test "#{report_type}_rule_parent_id_group" do
+            assert_match(/^xccdf_org.ssgproject.content_group_rpm_verification/,
+                         report.benchmark.rules[1].parent_id)
+          end
+          test "#{report_type}_rule_parent_type_with_group_parent" do
+            assert_match(/^Group/,
+                         report.benchmark.rules[1].parent_type)
+          end
+
+          test "#{report_type}_values" do
+            rule = report.benchmark.rules.select do |r|
+              r.id == 'xccdf_org.ssgproject.content_rule_accounts_password_pam_pwhistory_remember_password_auth'
+            end
+            assert_equal(['xccdf_org.ssgproject.content_value_var_password_pam_remember_control_flag',
+                          'xccdf_org.ssgproject.content_value_var_password_pam_remember'],
+                         rule[0].values)
+            assert_equal([], report.benchmark.rules[1].values)
+          end
+        end
+
+        context 'test result' do
+          test "#{report_type}_report_host" do
+            assert_match report.test_result.host, 'vm-rhel9'
+          end
+
+          test "#{report_type}_score can be parsed" do
+            assert_equal(89.149307, report.test_result.score)
+          end
+
+          context 'profiles' do
+            test "#{report_type}_test_result profile_id" do
+              assert_equal 'xccdf_org.ssgproject.content_profile_anssi_bp28_minimal',
+                           report.test_result.profile_id
+            end
+          end
+
+          context 'rules' do
+            test "#{report_type}_should parse rules for #{report_type} report" do
+              parse_rules report
+            end
+          end
+
+          context 'set values' do
+            test "#{report_type}_should parse set values for #{report_type} report" do
+              parse_set_values report
+            end
+          end
+        end
+
+        context 'fixes' do
+          test "#{report_type}_should parse fixes for #{report_type} report" do
+            parse_fixes report
+          end
+
+          test "#{report_type}_should parse multiple fixes for one rule" do
+            rule = report.benchmark.rules.find do |r|
+              r.id == 'xccdf_org.ssgproject.content_rule_ensure_gpgcheck_globally_activated'
+            end
+            fixes = rule.fixes
+            assert_equal 2, fixes.count
+            assert(fixes.map(&:id).all? { |id| id == 'ensure_gpgcheck_globally_activated' })
+            refute_equal fixes.first.system, fixes.last.system
+          end
+
+          test "#{report_type}_should parse one sub for fix" do
+            rule = report.benchmark.rules.find do |r|
+              r.id == 'xccdf_org.ssgproject.content_rule_selinux_state'
+            end
+            fix = rule.fixes.find { |f| !f.subs.empty? }
+            assert_equal 1, fix.subs.count
+            assert fix.subs.first.id
+            assert fix.subs.first.text
+          end
+
+          test "#{report_type}_should parse attributes for fix" do
+            rule = report.benchmark.rules.find do |r|
+              r.id == 'xccdf_org.ssgproject.content_rule_accounts_password_pam_dcredit'
+            end
+            fix = rule.fixes.find { |fx| fx.system == 'urn:xccdf:fix:script:sh' }
+            assert_equal 1, fix.subs.count
+            assert fix.text
+            assert fix.complexity
+            assert fix.disruption
+            assert fix.strategy
+          end
+
+          test "#{report_type}_should parse multiple subs for fix" do
+            rule = report.benchmark.rules.find do |r|
+              r.id == 'xccdf_org.ssgproject.content_rule_accounts_password_pam_pwhistory_remember_password_auth'
+            end
+            fix = rule.fixes.find { |f| !f.subs.empty? }
+            assert_equal 2, fix.subs.count
+            sub = fix.subs.last
+            assert sub.id
+            assert sub.text
+            assert sub.use
+          end
+
+          test "#{report_type}_should resolve set-values for subs" do
+            set_values = report.test_result.set_values
+            rule = report.benchmark.rules.find do |r|
+              r.id == 'xccdf_org.ssgproject.content_rule_selinux_state'
+            end
+            rule.fixes.first.map_child_nodes(set_values).all? { |node| node.is_a? Nokogiri::XML::Text }
+          end
+
+          test "#{report_type}_should parse full fix text lines" do
+            set_values = report.test_result.set_values
+            rule = report.benchmark.rules.find do |r|
+              r.id == 'xccdf_org.ssgproject.content_rule_selinux_state'
+            end
+            assert_equal 3, rule.fixes.first.full_text_lines(set_values).count
+          end
+
+          test "#{report_type}_should compose full fix" do
+            set_values = report.test_result.set_values
+            rule = report.benchmark.rules.find do |r|
+              r.id == 'xccdf_org.ssgproject.content_rule_selinux_state'
+            end
+            assert_equal file_fixture('ssh-rehl9-ds-selinux_full_fix.sh').read, rule.fixes.first.full_text(set_values)
+          end
+        end
+      end
+    end
+  end
+
   private
 
   def parse_fixes(result_file)
